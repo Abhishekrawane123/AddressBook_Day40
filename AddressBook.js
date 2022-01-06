8e0348efb551b560c9807975a76dcf2e2520ea89
pknowledge <abhishekrawane1195@gmail.com>
2022-01-07 00:25:58 +0530

    Added UC 3: Aray to insert conatct list

--- AddressBook.js
+++ AddressBook.js
@@ -1,17 +1,12 @@
-console.log("Welcome to address book program");
-// UC1-Create Address book contact
-class AddressBook{
-    //properties
-    firstName
-    lastName;
-    city;
-    address;
-    state;
-    zip;
-    mobnum;
-    email;
+const nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+const addressRegex = RegExp('^[a-zA-z0-9#,]{4,}$');
+const cityStateRegex = RegExp('^[a-zA-z]{4,}$');
+const zipRegex = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
+const phoneNumberRegex = RegExp("^[0-9]{2}\\s{1}[0-9]{10}$");
+const emailRegex = RegExp("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$");
 
-    //Constructor
+class ContactPerson{
+
     constructor(...params){
         this.firstName = params[0];
         this.lastName = params[1];
@@ -19,85 +14,113 @@ class AddressBook{
         this.city = params[3];
         this.state = params[4];
         this.zip = params[5];
-        this.mobnum = params[6];
+        this.phoneNumber = params[6];
         this.email = params[7];
-}
-    //method
-    toString() {
-        return "firstName: "+this.firstName +" , " + "lastName: "+this.lastName+" , " + "city: "
-        +this.city+" , " + "address: "+this.address+" , " + "state: "+this.state+" , " + "zip: "+this.zip
-        +" , " + "mobNumber: "+this.mobnum+" , " + "email: "+this.email;
     }
 
-    get firstName()
-    { 
-        return this._firstName
+    get firstName(){
+        return this._firstName;
     }
 
-    set firstName(firstName) {
-        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
-        if(nameRegex.test(firstName))
+    get lastName(){
+        return this._lastName;
+    }
+
+    get address(){
+        return this._address;
+    }
+
+    get city(){
+        return this._city;
+    }
+
+    get state(){
+        return this._state;
+    }
+
+    get zip(){
+        return this._zip;
+    }
+
+    get phoneNumber(){
+        return this._phoneNumber;
+    }
+
+    get email(){
+        return this._email;
+    }
+
+    set firstName(firstName){
+        if (nameRegex.test(firstName))
             this._firstName = firstName;
-        else throw "Incorrect First Name";
-    }
-    get lastName(){ return this._lastName}
-    set lastName(lastName){
-        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
-        if(nameRegex.test(lastName))
-            this._lastName = lastName
-        else throw "Incorrect Last Name"
-    }
-    get address(){ return this._address}
-    set address(address){
-        let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
-        if(regex.test(address))
-            this._address = address
-        else throw "Incorrect Address"
-    }
-    get city(){ return this._city}
-    set city(city){
-        let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
-        if(regex.test(city))
-            this._city = city
-        else throw "Incorrect City"
-    }
-    get state(){ return this._state}
-    set state(state){
-        let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
-        if(regex.test(state))
-            this._state = state
-        else throw "Incorrect State"
-    }
-    get zip(){ return this._zip}
-    set zip(zip){
-        
-        let pinRegex = RegExp('^[0-9]{3}[\\s]{0,1}[0-9]{3}$')
-        if( pinRegex.test(zip)){
-        
-            this._zip = zip
-        }
-        else throw "Incorrect Zip"
-    }
-    get phoneNo(){ return this._phoneNo}
-    set phoneNo(phoneNo){
-        let regex = RegExp('[0-9]{2}\\s[0-9]{10}')
-        if(regex.test(phoneNo))
-            this._phoneNo = phoneNo
-        else throw "Incorrect Phone Number"
-    }
-    get email(){ return this._email}
-    set email(email){
-        let regex = RegExp('^[a-z]+([_+-.]?[a-z0-9]+)*[@][a-z0-9]+[.]([a-z]+){2,}([.]?[a-z]{2})?$')
-        if(regex.test(email))
-            this._email = email
-        else throw "Incorrect Email"
+        else
+            throw "FIRST NAME is Incorrect ";
     }
 
+    set lastName(lastName){
+        if (nameRegex.test(lastName))
+            this._lastName = lastName;
+        else
+            throw " LAST NAME is Incorrect ";
+    }
+
+    set address(address){
+        if (addressRegex.test(address))
+            this._address = address;
+        else
+            throw "ADDRESS is Incorrect";
+    }
+
+    set city(city){
+        if (cityStateRegex.test(city))
+            this._city = city;
+        else
+            throw "CITY is Incorrect";
+    }
+
+    set state(state){
+        if (cityStateRegex.test(state))
+            this._state = state;
+        else
+            throw " STATE is Incorrect ";
+    }
+
+    set zip(zip){
+        if (zipRegex.test(zip))
+            this._zip = zip;
+        else
+            throw " ZIP is Incorrect";
+    }
+
+    set phoneNumber(phoneNumber){
+        if (phoneNumberRegex.test(phoneNumber))
+            this._phoneNumber = phoneNumber;
+        else
+            throw " PHONE NUMBER is Incorrect ";
+    }
+
+    set email(email){
+        if (emailRegex.test(email))
+            this._email = email;
+        else
+            throw "EMAIL ADDRESS is Incorrect ";
+    }
+    
+    toString(){
+        return "First Name : "+ this.firstName + ", Last Name : "+ this.lastName + ", Address : " + this.address + ", City : "+ this.city + ", State : "+ this.state +", Zip : "+ this.zip+ ", Phone Number : "+ this.phoneNumber + ", Email : "+ this.email;
+    }
 }
+let contactArray=new Array();
 try{
-let addressbook = new AddressBook("Abhishek","Rawane","Akola","Gajanan Nagar","maharashtra",444001,8208629118,"Arawane@gmail.com")
-console.log(addressbook.toString());
+
+
+    let contact = new ContactPerson("Abhi", "Rawane", "26048", "Akola", "Maharashtra", "444 001", "91 9481555555", "AbhiRawane@gmail.com");
+    contactArray.push(contact);
+    
+    contactArray.push( new ContactPerson("Manish","Jangid","45678","Sikar","Rajasthan","789 233","91 8970654321","ManishJangid@gmail.com"));
+   
 }
 catch(e){
-    console.log(e)
+    console.log(e);
 }
+console.log(contactArray.toString());
